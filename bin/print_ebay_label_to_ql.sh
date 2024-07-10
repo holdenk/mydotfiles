659394f3dfb198b8712c949fb4b5f0bcb7aac3a3
Holden Karau <holden@pigscanfly.ca>
2024-07-09 19:50:08 -0700

    Ok and mini rewrite

--- bin/print_ebay_label_to_ql.sh
+++ bin/print_ebay_label_to_ql.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ex
-if ! command -v pdfScale.sh &> /dev/null
-then
-  git clone git@github.com:tavinus/pdfScale.git
-  cp pdfScale/*.sh ~/.bin/
-fi
 
 qpdf $1 rotated.pdf --rotate=-90:1-z
 pdfcrop rotated.pdf cropped.pdf
-pdfScale.sh  rotated.pdf  out.pdf -r "custom mm 103 164" -s 0.85 --no-fit-to-page --hor-align left --vert-align bottom
-lp  -d Brother_QL_1110NWB out.pdf 
+lp  -d Brother_QL_1110NWB cropped.pdf 
