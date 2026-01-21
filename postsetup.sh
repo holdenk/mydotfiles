78c4b4cbe43e0f95aba9c0107aab2793d64488bf
Holden Karau <holden@pigscanfly.ca>
2026-01-21 15:24:16 -0800

    Cleanup some post setup stuff

--- postsetup.sh
+++ postsetup.sh
@@ -3,19 +3,19 @@
 set -x
 mkdir -p ~/repos
 pushd ~/repos
-CNTX=users; NAME=holdenk;
-for PAGE in {1..5}; do
-  REPOS=$(curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
-            grep -e 'git_url*' |
- 	    cut -d \" -f 4)
-  for REPO in ${REPOS[@]}; do 
-    git clone $REPO || echo "Already cloned $REPO"
-  done
-done
+#CNTX=users; NAME=holdenk;
+#for PAGE in {1..5}; do
+#  REPOS=$(curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
+#            grep -e 'git_url*' |
+# 	    cut -d \" -f 4)
+#  for REPO in ${REPOS[@]}; do 
+#    git clone $REPO || echo "Already cloned $REPO"
+#  done
+#done
 popd
 set -ex
 # More bluetooth stuff
-sudo apt-get install bluez*
+sudo apt-get install -y bluez*
 #tailscale
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
 
@@ -40,8 +40,14 @@ curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89
 
 # Tailscale installation using official install script
 curl -fsSL https://tailscale.com/install.sh | sh
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+# sbt
+sudo apt-get install apt-transport-https curl gnupg -yqq
+echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
+echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
+curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo -H gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/scalasbt-release.gpg --import
+sudo chmod 644 /etc/apt/trusted.gpg.d/scalasbt-release.gpg
 sudo apt-get update
+sudo apt-get install -y sbt
 # Skip tailscale because of potential conflicts
 #sudo apt-get install tailscale sbt
 #if [ -z "$SERVER" ]; then
@@ -49,75 +55,6 @@ sudo apt-get update
 #fi
 wget https://dl.google.com/go/go1.13.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xvf go1.13.1.linux-amd64.tar.gz
-sudo pip install Pygments sphinx pypandoc mkdocs &
 
-# Try to find available gem version dynamically
-GEM_CMD=""
-for gem_path in /usr/bin/gem*; do
-  if [ -x "$gem_path" ]; then
-    GEM_CMD="$gem_path"
-    break
-  fi
-done
-
-if [ -n "$GEM_CMD" ]; then
-  sudo $GEM_CMD install jekyll pygments.rb &
-else
-  echo "Warning: No gem command found, skipping jekyll/pygments.rb installation"
-fi
-
-sudo Rscript -e 'install.packages(c("knitr", "devtools", "roxygen2", "testthat", "rmarkdown"), repos="http://cran.stat.ucla.edu/")' &
-gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -L https://get.rvm.io | bash -s stable
-source ~/.rvm/scripts/rvm
-rvm install 2.4.2 &&
-wait
-rvm use ruby-2.4.2
-gem install jekyll pygments.rb &
-#echo "Installing ffmpeg"
-#pushd /tmp
-#git clone --depth 1 git://source.ffmpeg.org/ffmpeg.git
-#pushd ffmpeg
-#./configure --enable-shared --prefix=/usr
-#make -j4
-#sudo checkinstall --pkgname=FFmpeg --fstrans=no --backup=no \
-#     --pkgversion="$(date +%Y%m%d)-git" --deldoc=yes
-#popd
-#popd
-mkdir ~/bin
-if [ -z "$SERVER" ]; then
-   pushd /tmp
-   wget https://github.com/ksonnet/ksonnet/releases/download/v0.9.1/ks_0.9.1_linux_amd64.tar.gz
-   tar -xvf ks_0.9.1_linux_amd64.tar.gz
-   mv ks_0.9.1_linux_amd64/ks ~/bin/
-   popd
-   # Bluez
-   mkdir -p /tmp/bluez
-   pushd /tmp/bluez
-   wget http://www.kernel.org/pub/linux/bluetooth/bluez-5.54.tar.xz
-   tar -xvf bluez-5.54.tar.xz
-   cd bluez-5.54
-   ./configure
-   make
-   sudo make install
-   mkdir -p /tmp/libbtbb
-   pushd /tmp/libbtbb
-   wget https://github.com/greatscottgadgets/libbtbb/archive/2018-12-R1.tar.gz -O libbtbb-2018-12-R1.tar.gz
-   tar xf libbtbb-2018-12-R1.tar.gz
-   cd libbtbb-2018-12-R1
-   export ENABLE_PYTHON=true
-   mkdir build
-   cd build
-   cmake ..
-   make
-   sudo make install
-   sudo ldconfig
-fi
-# ssh keys
 GH_USER=${GH_USER:-holdenk}
 curl https://github.com/${GH_USER}.keys | ${RUN_DEST_CMD} tee -a ~/authorized_keys
-# TODO: Disable password login
-wget https://download.brother.com/welcome/dlf006893/linux-brprinter-installer-2.2.3-1.gz
-gunzip linux-brprinter-installer-2.2.3-1.gz
-chmod a+x linux-brprinter-installer-2.2.3-1
-./linux-brprinter-installer-2.2.3-1.gz
